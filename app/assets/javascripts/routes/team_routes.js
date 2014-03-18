08f3614af6981af54fe9073157ffee89705917c9
Scott Mascio <scottmascio@gmail.com>
2014-03-18 09:21:54 -0400

    links to individual teams from the tourament show page

--- app/assets/javascripts/routes/team_routes.js
+++ app/assets/javascripts/routes/team_routes.js
@@ -3,3 +3,9 @@ TournyMadness.TeamsRoute = Ember.Route.extend({
     return this.store.find('team');
   }
 });
+
+TournyMadness.TeamRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('team');
+  }
+});
